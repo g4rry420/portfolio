76c66ccecd36d50e477829e6788e274773fe8925
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-21 18:42:54 -0500

    technologies are added

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,16 +2,16 @@ import React,{ useState } from 'react'
 
 export default function Projects() {
     const [content, setContent] = useState([
-        { id: 2, imgUrl: `${require("../../images/comfyHouse.PNG")}`, webUrl: "https://comfy-house.g4rry420.vercel.app/", title: "Comfy House - An Ecommerce Website" },
-        { id: 3, imgUrl: `${require("../../images/spotidex.PNG")}`, webUrl: "https://spotidex.vercel.app/#", title: "Spotidex - A Music app" },
-        { id: 8, imgUrl: `${require("../../images/messaggio.PNG")}`, webUrl: "https://messaggio.vercel.app/", title: "Messaggio - A Chat Application" },
-        { id: 9, imgUrl: `${require("../../images/dev-world.PNG")}`, webUrl: "https://dev-world.vercel.app/", title: "Dev World - A Social Media Application" },
-        { id: 10, imgUrl: `${require("../../images/alkaline.PNG")}`, webUrl: "https://alkaline.vercel.app/", title: "Alkaline - A Video Calling Application" },
-        { id: 1, imgUrl: `${require("../../images/countries.png")}`, webUrl: "https://rest-countries-api-with-color-theme-switcher-jet.now.sh/", title: "Rest Countries API made using vanilla javascript."},
-        { id: 4, imgUrl: `${require("../../images/json.PNG")}`, webUrl: "https://json-validator-and-formatter.vercel.app/", title: "Json Formatter and Validator" },
-        { id: 5, imgUrl: `${require("../../images/passwordGenerator.PNG")}`, webUrl: "https://password-generator-dun-sigma.vercel.app/", title: "Password Generator" },
-        { id: 6, imgUrl: `${require("../../images/huddle.PNG")}`, webUrl: "https://huddle-landing-page-roan-ten.now.sh/", title: "Huddle - A Landing Page" },
-        { id: 7, imgUrl: `${require("../../images/socialMedia.PNG")}`, webUrl: "https://social-media-dashboard-switcher.now.sh/#", title: "A Social Media Dashboard Page"}
+        { id: 2, imgUrl: `${require("../../images/comfyHouse.PNG")}`, webUrl: "https://comfy-house.g4rry420.vercel.app/", title: "Comfy House - An Ecommerce Website", tech: "JavaScript, React and Firebase" },
+        { id: 3, imgUrl: `${require("../../images/spotidex.PNG")}`, webUrl: "https://spotidex.vercel.app/#", title: "Spotidex - A Music app", tech: "React and Spotify API" },
+        { id: 8, imgUrl: `${require("../../images/messaggio.PNG")}`, webUrl: "https://messaggio.vercel.app/", title: "Messaggio - A Chat Application", tech: "JavaScript, React and Firebase" },
+        { id: 9, imgUrl: `${require("../../images/dev-world.PNG")}`, webUrl: "https://dev-world.vercel.app/", title: "Dev World - A Social Media Application", tech: "JavaScript, React and Firebase" },
+        { id: 10, imgUrl: `${require("../../images/alkaline.PNG")}`, webUrl: "https://alkaline.vercel.app/", title: "Alkaline - A Video Calling Application", tech: "JavaScript, React and WebRTC" },
+        { id: 1, imgUrl: `${require("../../images/countries.png")}`, webUrl: "https://rest-countries-api-with-color-theme-switcher-jet.now.sh/", title: "Rest Countries API made using vanilla javascript.", tech: "JavaScript"},
+        { id: 4, imgUrl: `${require("../../images/json.PNG")}`, webUrl: "https://json-validator-and-formatter.vercel.app/", title: "Json Formatter and Validator", tech: "JavaScript and React" },
+        { id: 5, imgUrl: `${require("../../images/passwordGenerator.PNG")}`, webUrl: "https://password-generator-dun-sigma.vercel.app/", title: "Password Generator", tech: "Javacript and React" },
+        { id: 6, imgUrl: `${require("../../images/huddle.PNG")}`, webUrl: "https://huddle-landing-page-roan-ten.now.sh/", title: "Huddle - A Landing Page", tech: "HTML and CSS" },
+        { id: 7, imgUrl: `${require("../../images/socialMedia.PNG")}`, webUrl: "https://social-media-dashboard-switcher.now.sh/#", title: "A Social Media Dashboard Page", tech: "HTML and CSS"}
     ])
 
 
@@ -20,27 +20,6 @@ export default function Projects() {
             <h2 className="display-3 py-5">Projects</h2>
             <div className="container">
                 <div className="row fade-out-siblings">
-                {/*
-                    content.map(item => (
-                        <div className="col-md-4 col-12 mb-5" key={item.id}>
-                            <div className="card cards">
-                                <img className="card-img-top" src={item.imgUrl} alt="card"/>
-                                <div className="overlay">
-                                    <div className="text-center">
-                                        <h5> {item.title} </h5>
-                                    </div> 
-                                    <div className="link">
-                                        <a id="website" href={item.webUrl} target="_blank" rel="noopener noreferrer">
-                                            <button className="btn">
-                                                View Website
-                                            </button>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                */}
                 {
                     content.map(item => (
                         <div  className="col-md-4 flex-item  col-12 mb-5" key={item.id}>
@@ -48,9 +27,10 @@ export default function Projects() {
                                 <img src={item.imgUrl} alt="card" className="card-img-top"/>
                                 <div className="card-body">
                                     <h5 className="card-title">{item.title}</h5>
-                                    <p className="card-text"></p>
+                                    <p className="card-text display-4">Technologies: {item.tech}</p>
                                     <a href={item.webUrl} target="_blank" rel="noopener noreferrer" className="btn btn-website">View Website</a>
                                 </div>
+
                             </div>
                         </div>
                     ))
