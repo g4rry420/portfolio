0a4dccdb29b79b7f68b9eb404f6f6ec3e28d3d10
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-21 18:48:04 -0500

    fixed a typo in technologies

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,13 +3,13 @@ import React,{ useState } from 'react'
 export default function Projects() {
     const [content, setContent] = useState([
         { id: 2, imgUrl: `${require("../../images/comfyHouse.PNG")}`, webUrl: "https://comfy-house.g4rry420.vercel.app/", title: "Comfy House - An Ecommerce Website", tech: "JavaScript, React and Firebase" },
-        { id: 3, imgUrl: `${require("../../images/spotidex.PNG")}`, webUrl: "https://spotidex.vercel.app/#", title: "Spotidex - A Music app", tech: "React and Spotify API" },
+        { id: 3, imgUrl: `${require("../../images/spotidex.PNG")}`, webUrl: "https://spotidex.vercel.app/#", title: "Spotidex - A Music app", tech: "JavaScript, React and Spotify API" },
         { id: 8, imgUrl: `${require("../../images/messaggio.PNG")}`, webUrl: "https://messaggio.vercel.app/", title: "Messaggio - A Chat Application", tech: "JavaScript, React and Firebase" },
         { id: 9, imgUrl: `${require("../../images/dev-world.PNG")}`, webUrl: "https://dev-world.vercel.app/", title: "Dev World - A Social Media Application", tech: "JavaScript, React and Firebase" },
         { id: 10, imgUrl: `${require("../../images/alkaline.PNG")}`, webUrl: "https://alkaline.vercel.app/", title: "Alkaline - A Video Calling Application", tech: "JavaScript, React and WebRTC" },
         { id: 1, imgUrl: `${require("../../images/countries.png")}`, webUrl: "https://rest-countries-api-with-color-theme-switcher-jet.now.sh/", title: "Rest Countries API made using vanilla javascript.", tech: "JavaScript"},
         { id: 4, imgUrl: `${require("../../images/json.PNG")}`, webUrl: "https://json-validator-and-formatter.vercel.app/", title: "Json Formatter and Validator", tech: "JavaScript and React" },
-        { id: 5, imgUrl: `${require("../../images/passwordGenerator.PNG")}`, webUrl: "https://password-generator-dun-sigma.vercel.app/", title: "Password Generator", tech: "Javacript and React" },
+        { id: 5, imgUrl: `${require("../../images/passwordGenerator.PNG")}`, webUrl: "https://password-generator-dun-sigma.vercel.app/", title: "Password Generator", tech: "JavaScript and React" },
         { id: 6, imgUrl: `${require("../../images/huddle.PNG")}`, webUrl: "https://huddle-landing-page-roan-ten.now.sh/", title: "Huddle - A Landing Page", tech: "HTML and CSS" },
         { id: 7, imgUrl: `${require("../../images/socialMedia.PNG")}`, webUrl: "https://social-media-dashboard-switcher.now.sh/#", title: "A Social Media Dashboard Page", tech: "HTML and CSS"}
     ])
